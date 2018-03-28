4a5c028a5b2f8e139522873e50a75a45656c5071
wdyogesh <wdyogesh@gmail.com>
2018-03-28 17:27:22 +0530

    ad data

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database-deprecated';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [AngularFireDatabase]
+})
+export class AppComponent implements OnInit {
+  title = 'Angular Firebase Tutorial';
+  user = {};
+  users: FirebaseListObservable<any[]>;
+  constructor(private db: AngularFireDatabase) {}
+
+  ngOnInit() {
+    this.users = this.db.list('/users');
+  }
+
+  onSubmit() {
+    this.users.push(this.user);
+    this.user = {};
+  }
+
+  deleteUser(key: string){
+    if (confirm('Are you sure to delete this record ?') == true) {
+      this.users.remove(key);
+      this.user = {};
+    }
+  }
+
+  editUser(key: string){
+    
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+
+import { NgModule } from '@angular/core';
+
+import {FormsModule} from '@angular/forms';
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+import { AppComponent } from './app.component';
+
+import {environment} from '../environments/environment';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.config), //Initialize firebase
+    AngularFireDatabaseModule, // for database
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
